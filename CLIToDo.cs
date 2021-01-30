e5a887c401d8b79414009209f5d158558e2b7cce
oikz <chapmanjgm@gmail.com>
2021-01-30 19:24:22 +1300

    ID Stuffs and setup

--- CLIToDo.cs
+++ CLIToDo.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using Microsoft.Graph;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
 class CLIToDo {
+    //Global Variable Goodies
+    public static string tenantID = "fce5109a-b406-449c-8c1e-48319af5ab15";
+    public static string appID = "0123aef1-0156-455a-a2ce-1e59d4595e79";
+    public static GraphServiceClient graphClient;
     static void Main(string[] args) {
 
-        Console.WriteLine("Hello World!");
     }
 }
