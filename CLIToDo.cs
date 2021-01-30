4911960ae75eabbe5a5efb9bdb99a683bdd2f663
oikz <chapmanjgm@gmail.com>
2021-01-30 21:06:52 +1300

    Fixed up Date, added Time and made moar methods

--- CLIToDo.cs
+++ CLIToDo.cs
@@ -5,11 +5,18 @@ using System.Threading.Tasks;
 
 class CLIToDo {
     //Global Variable Goodies
-    public static string tenantID = "fce5109a-b406-449c-8c1e-48319af5ab15";
+    public static string tenantID = "fce5109a-b406-449c-8c1e-48319af5ab15";//Spooky tenantID
     public static string appID = "0123aef1-0156-455a-a2ce-1e59d4595e79";
     public static string[] graphScopes;
     public static GraphServiceClient graphClient;
+
     static void Main(string[] args) {
+        CLIToDo instance = new CLIToDo();
+        instance.start();
+    }
+
+    //Start the program
+    private void start() {
         graphScopes = new[] { "User.Read", "Tasks.ReadWrite" };
 
 
@@ -20,43 +27,48 @@ class CLIToDo {
         Console.WriteLine("Date: Format: YYYY-MM-DD (Empty for today)");
         //Cursed
         string date;
-        DateTime newDate;
+
+        //VS was complaining about uninitialized vars
+        DateTime newDate = new DateTime();
         DateTime newTime = new DateTime();
-        while (true) {
 
-            date = Console.ReadLine();
-            if (date == "") {
-                date = DateTime.Today.ToString("yyyy/mm/dd");
-                break;
-            }
-            try {
-                newDate = Convert.ToDateTime(date);
-                break;
-            }
-            catch {
-                Console.WriteLine("Try Again");
-            }
-
-        }
+        newDate = getDate();
         Console.WriteLine("Time: ");
-        while (true) {
+        newTime = getTime();
 
-            string time = Console.ReadLine();
-            /*try {
-                newTime.);
-                Console.WriteLine(newTime.ToString());
-                break;
-            }
-            catch {
-                Console.WriteLine("Try Again");
-                Console.WriteLine(newTime.ToString());
-            }*/
-
-        }
         DateTimeTimeZone reminderTime = new DateTimeTimeZone();
         reminderTime.TimeZone = "Pacific/Auckland";
-        //reminderTime.DateTime =
-        //newTask.ReminderDateTime =
+        reminderTime.DateTime = newDate.ToString() + "T" + newTime.ToString();
+        newTask.ReminderDateTime = reminderTime;
 
+    }
+
+    //Separate Methods for niceness
+    static private DateTime getDate() {
+        string date = Console.ReadLine();
+        if (date == "") {
+            return DateTime.Today;
+        }
+        try {
+            DateTime newDate = Convert.ToDateTime(date);
+            return newDate;
+        }
+        catch {
+            Console.WriteLine("Try Again");
+            return getDate();
+        }
+    }
+
+    static private DateTime getTime() {
+        string time = Console.ReadLine();
+        DateTime newTime;
+        try {
+            newTime = Convert.ToDateTime(time);
+            return newTime;
+        }
+        catch {
+            Console.WriteLine("Try Again");
+            return getTime();
+        }
     }
 }
