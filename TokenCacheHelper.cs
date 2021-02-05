a1f31a21ea2db0c18f102976023bbf83e68c8511
oikz <chapmanjgm@gmail.com>
2021-02-05 14:00:12 +1300

    Remove protectedData to support non-windows

--- TokenCacheHelper.cs
+++ TokenCacheHelper.cs
@@ -8,31 +8,27 @@ static class TokenCacheHelper {
         tokenCache.SetBeforeAccess(BeforeAccessNotification);
         tokenCache.SetAfterAccess(AfterAccessNotification);
     }
-    
+
     public static readonly string CacheFilePath =
         System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalcache.bin3";
 
     private static readonly object FileLock = new object();
-    
+
     private static void BeforeAccessNotification(TokenCacheNotificationArgs args) {
         lock (FileLock) {
             args.TokenCache.DeserializeMsalV3(File.Exists(CacheFilePath)
-                ? ProtectedData.Unprotect(File.ReadAllBytes(CacheFilePath),
-                    null,
-                    DataProtectionScope.CurrentUser)
+                ? File.ReadAllBytes(CacheFilePath)
                 : null);
         }
     }
+
     private static void AfterAccessNotification(TokenCacheNotificationArgs args) {
         // if the access operation resulted in a cache update
         if (args.HasStateChanged) {
             lock (FileLock) {
                 // reflect changes in the persistent store
                 File.WriteAllBytes(CacheFilePath,
-                    ProtectedData.Protect(args.TokenCache.SerializeMsalV3(),
-                        null,
-                        DataProtectionScope.CurrentUser)
-                );
+                    args.TokenCache.SerializeMsalV3());
             }
         }
     }
