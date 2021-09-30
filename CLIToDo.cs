52ff3c674d45fc1decb24e185366d8b220bb7ce2
oikz <chapmanjgm@gmail.com>
2021-09-30 21:37:56 +1300

    Fix for creating reminders on months 10-12

--- CLIToDo.cs
+++ CLIToDo.cs
@@ -100,7 +100,7 @@ class CLIToDo {
                 return newDate.Year + "-0" + newDate.Month + "-" + newDate.Day;
             }
 
-            return newDate.Year + "0" + newDate.Month + "-" + newDate.Day;
+            return newDate.Year + "-" + newDate.Month + "-" + newDate.Day;
         }
 
         //Attempt to create a DateTime based on the user's inputs
